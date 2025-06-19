277bd3c948e16e19e8d076df40407aeebc90aea7
Shubh Chincholkar <shubhchincholkar88@gmail.com>
2025-06-19 20:41:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// todo: Make M+  M- and Mc functional
+let string = "";
+let button = document.querySelectorAll('.button');
+ Array.from( button).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if (e.target.innerHTML == '='){
+            string = eval(string)
+            document.querySelector('input').value = string;
+        }
+
+        else if (e.target.innerHTML == 'C'){
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else{
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+    }
+    })
+
+ })
+
+ var dulha = "lab";
+ var dulhan = "laby";
+       
